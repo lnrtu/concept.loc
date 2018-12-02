137965373698aa48e5c1dcb93e0dd952fdd74c21
lnrtu <orestrov@gmail.com>
2018-12-02 13:18:17 +0200

    Create Admin middleware

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +20,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->middleware(['auth', 'admin'])->group(function(){
+    Route::get('/', function (){
+        return 123;
+    });
+});
+
